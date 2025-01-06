f678d64cdb569c6e03cc15d931d832619f432058
Mukul Gupta <guptamukul2511@gmail.com>
2025-01-06 19:58:26 +0530

    6-1-2025

--- pos_of_only_Setbit.cpp
+++ pos_of_only_Setbit.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){ 
-    
-    return 0; 
+int pos_of_only_setbit(int n){
+    if(n<=0 || (n&(n-1))!=0){
+        return -1;
+    }
+    return (log2(n)+1);
+}
+int main(){
+    int n;
+    cin>>n;
 
-}       
+    int pos=pos_of_only_setbit(n);
+    if(pos==-1) cout<<"No. is either zero or not power of 2 ";
+    else cout<<"position of only set bit is "<<pos;
+
+    return 0;
+}
--- pow_of_2.cpp
+++ pow_of_2.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){ 
-    
-    return 0; 
+bool pow_of_two(int n){
+    return (n>0) && (n&(n-1))==0;
+}
+int main(){
+    int n;
+    cin>>n;
 
+    if(pow_of_two(n)){
+        cout<<n<<"is power of 2 ";
+    }
+    else{
+        cout<<n<<"is not power of 2 ";
+    }
+
+    return 0;
 }
