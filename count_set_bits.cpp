295c257d9535a77831e391613996a6c8b63fef12
Mukul Gupta <guptamukul2511@gmail.com>
2025-01-06 19:33:17 +0530

    6-12-2025

--- count_set_bits.cpp
+++ count_set_bits.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+int count_set_bits(int n) {
+    int count = 0;
+    while(n){
+        n=n&(n - 1);
+        count++;
+    }
+    return count;
+}
 
-int main(){ 
+int main() {
+    int n;
+    cout<<"Enter an integer: ";
+    cin>> n;
+    cout<<"Number of set bits: " << count_set_bits(n) << endl;
     
-    return 0; 
-
+    return 0;
 }
